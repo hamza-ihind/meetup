74bea3e03a61f4ec0020c6f0ef43adbbaefc4371
hamza-ihind <ihind.mizo@gmail.com>
2023-07-29 22:42:03 +0100

    made the logic of login/register

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
-const gql = require("graphql-tag");
 const mongoose = require("mongoose");
+
+const typeDefs = require("./graphql/typeDefs");
+const resolvers = require("./graphql/resolvers");
 
 // Destructured
 const { ApolloServer } = require("apollo-server");
@@ -7,31 +9,6 @@ const { MONGODB } = require("./config.js");
 
 // models
 const Post = require("./models/Post.model.js");
-
-const typeDefs = gql`
-  type Post {
-    id: ID!
-    body: String!
-    createdAt: String!
-    username: String!
-  }
-  type Query {
-    getPosts: [Post]
-  }
-`;
-
-const resolvers = {
-  Query: {
-    async getPosts() {
-      try {
-        const posts = await Post.find();
-        return posts;
-      } catch (err) {
-        throw new Error(err);
-      }
-    },
-  },
-};
 
 const server = new ApolloServer({
   typeDefs,
