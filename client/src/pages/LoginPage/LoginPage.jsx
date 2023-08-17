20a67c19f1b5bba4240720765ce9f48565528e42
hamza-ihind <ihind.mizo@gmail.com>
2023-08-17 04:01:01 +0100

    added register/login fetching data and componenets

--- client/src/pages/LoginPage/LoginPage.jsx
+++ client/src/pages/LoginPage/LoginPage.jsx
@@ -1,18 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
+import gql from "graphql-tag";
+import { useMutation } from "@apollo/client";
+import useForm from "../../hooks/useForm";
 
 // Import styles
 import "./LoginPage.scss";
 
 const LoginPage = () => {
+  const { onChange, onSubmit, values } = useForm(loginUserCallback, {
+    username: "",
+    password: "",
+  });
+
+  const [loginUser, { loading }] = useMutation(LOGIN_USER, {
+    update(proxy, result) {
+      console.log(result);
+    },
+    variables: values,
+  });
+
+  function loginUserCallback() {
+    loginUser();
+  }
+
   return (
     <div className="login">
       <h2> Login </h2>
-      <form>
-        <input placeholder="email" type="email" name="email" />
-        <input placeholder="password" type="password" name="password" />
+      <form noValidate onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={values.username}
+          placeholder="username"
+          type="text"
+          name="username"
+        />
+        <input
+          onChange={onChange}
+          value={values.password}
+          placeholder="password"
+          type="password"
+          name="password"
+        />
+        <button type="submit">Log in</button>
       </form>
     </div>
   );
 };
+
+const LOGIN_USER = gql`
+  mutation login($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
+      id
+      email
+      username
+      createdAt
+      token
+    }
+  }
+`;
 
 export default LoginPage;
--- client/src/pages/RegisterPage/RegisterPage.jsx
+++ client/src/pages/RegisterPage/RegisterPage.jsx
@@ -1,10 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
+import gql from "graphql-tag";
+import { useMutation } from "@apollo/client";
+import useForm from "../../hooks/useForm";
 
 // Import styles
 import "./RegisterPage.scss";
 
 const RegisterPage = () => {
-  return <div>RegisterPage</div>;
+  const { onChange, onSubmit, values } = useForm(registerUser, {
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const [addUser, { loading }] = useMutation(REGISTER_USER, {
+    update(proxy, result) {
+      console.log(result);
+    },
+    variables: values,
+  });
+
+  function registerUser() {
+    addUser();
+  }
+
+  return (
+    <div className="register">
+      <h2> Register </h2>
+      <form onSubmit={onSubmit} noValidate>
+        <input
+          onChange={onChange}
+          value={values.username}
+          placeholder="username"
+          type="text"
+          name="username"
+        />
+        <input
+          onChange={onChange}
+          value={values.email}
+          placeholder="email"
+          type="email"
+          name="email"
+        />
+        <input
+          onChange={onChange}
+          value={values.password}
+          placeholder="password"
+          type="password"
+          name="password"
+        />
+        <input
+          onChange={onChange}
+          value={values.confirmPassword}
+          placeholder="Confirm password"
+          type="password"
+          name="confirmPassword"
+        />
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
 };
+
+const REGISTER_USER = gql`
+  mutation register(
+    $username: String!
+    $email: String!
+    $password: String!
+    $confirmPassword: String!
+  ) {
+    register(
+      registerInput: {
+        username: $username
+        email: $email
+        password: $password
+        confirmPassword: $confirmPassword
+      }
+    ) {
+      id
+      email
+      username
+      createdAt
+      token
+    }
+  }
+`;
 
 export default RegisterPage;
